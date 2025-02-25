cc5a8f37c45055903e8080fbbdd69b83209866a5
j4jigs <j4jigs@gmail.com>
2025-02-25 16:46:31 +0530

    This is my first commit

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "github-actions-demo-9876"
+    bucket = "github-actions-demo-98769"
     key    = "github-actions-demo.tfstate"
     region = "us-east-1"
   }
