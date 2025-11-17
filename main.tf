a65c89f7ccef471ca74a87e4844a7993de894ba7
Jignesh Patel <talktojignesh@gmail.com>
2025-11-17 21:46:45 +0530

    updated

--- main.tf
+++ main.tf
@@ -21,10 +21,10 @@ resource "aws_security_group" "ec2_sg" {
 
 # EC2 Instance (No input required)
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0ecb62995f68bb549"   # Amazon Linux 2 (Mumbai)
-  instance_type = "t2.micro"                # DEFAULT (no user input)
+  ami           = "ami-0ecb62995f68bb549" # Amazon Linux 2 (Mumbai)
+  instance_type = "t2.micro"              # DEFAULT (no user input)
 
-#  key_name        = aws_key_pair.mykey.key_name
+  #  key_name        = aws_key_pair.mykey.key_name
   security_groups = [aws_security_group.ec2_sg.name]
 
   tags = {
--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "asdasdwqeqw23221asas"   # must be globally unique & lowercase
+  bucket = "asdasdwqeqw23221asas" # must be globally unique & lowercase
 
   tags = {
     Name = "MyBucket"
