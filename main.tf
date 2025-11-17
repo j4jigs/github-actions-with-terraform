4c841a78f2b3ad630bed8f3a9d8a3d3b3cd5ee00
Jignesh Patel <talktojignesh@gmail.com>
2025-11-17 21:27:44 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
 # Create a key pair (optional: update the public_key path if needed)
-resource "aws_key_pair" "mykey" {
-  key_name   = "mykey"
-#  public_key = file("~/.ssh/id_rsa.pub")
-}
-
 # Security Group allowing SSH
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2-sg"
