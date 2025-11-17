dcdc56fe75d51349916dda47dc8b0db4fc76e4da
Jignesh Patel <talktojignesh@gmail.com>
2025-11-17 22:40:18 +0530

    updated

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-south-1a", "ap-south-1b"]
+  azs             = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
 
